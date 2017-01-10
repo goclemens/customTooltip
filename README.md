9c9fe1c9283bae34cd753dae54a25cc8faab77d9
Clemens Huebner-Worseck <huebner-worseck@zib.de>
2017-01-10 12:04:29 +0100

    first version

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # customTooltip
+
+CTooltip is an object constructor for a customizable tooltip. The tooltip consists of three parts. A heading (top) a list with metainfos (left) and a svg (right) all three can be fully cusomized by setting functions to get them or build them from the selected (hovered) htmlElement.
+
+Usage:
+1. Initialize an object: "var tooltip = new CTooltip"
+2. Add the tooltip to htmlElements by "tooltip.addTo(htmlElement)". The tooltip will be shown "onMouseEnter(event)". Default is "event.target.id".
+3. Set a function to get a heading with "tooltip.setHeading(function)". The function will get "event" as argument from the "onMouseEnter(event)" event.
+4. Set a function to get metainfos with "tooltip.setMetainfos(function)". The function will get "event" as argument from the "onMouseEnter(event)" event.
+5. Set a function to get a SVG with "tooltip.setSvg(function)". The function will get "event" as argument from the "onMouseEnter(event)" event.
+
+Additions:
+- the tooltip can be removed from htmlElements by "tooltip.removeFrom(htmlElement)"
+- setHeading function has to return a string object
+- setMetainfos function has to return a js-object { firstInfo : "first", secondInfo : "second" ...}
+- setSvg function has to return an svg element (node)
+- style of the container element ("div") can be customized by "tooltip.setStyle(styles)" (styeles is a js object)
+
+CSS-classes:
+div container: "ctooltip"
+div heading: "ctooltip_heading"
+ul metainfos: "ctooltip_meta"
+div svgContainer: "ctooltip_svg"
---
+++ js/CTooltip.js
@@ -0,0 +1,191 @@
+function CTooltip(id) {
+
+  //#### CONSTRUCTOR START ####
+  //#### definition & initializing of tooltip object ####
+
+  //------- initialization of container -------
+  var container = document.createElement("div");
+
+  // optional contructor argument
+  if (id !== undefined) {
+    container.id = id;
+  }
+
+  container.className = "ctooltip";
+  container.style.display = "none";
+  container.style["pointer-events"] = "none";
+  container.style.position = "absolute";
+
+  //------- default styles for container -------
+  container.style.background = "rgba(136, 229, 252, 0.9)";
+  container.style.border = "2px solid #00cbff";
+  container.style["border-radius"] = "20px";
+  container.style.padding = "10px";
+  //container.style.height = "200px";
+  container.style["min-width"] = "350px";
+  container.style["font-family"] = "Tahoma, Geneva, sans-serif";
+
+
+  //------- tooltip parts -------
+
+  // heading
+  var heading = document.createElement("div");
+  heading.className = "ctooltip_heading";
+  heading.innerHTML = "";
+  container.appendChild(heading);
+
+  heading.style["font-weight"] = "bold";
+  heading.style["font-size"] = 28;
+  heading.style["padding-bottom"] = "5px";
+  heading.style.background = "linear-gradient(to bottom, rgb(0, 199, 255), rgb(136, 229, 252))"
+
+  // metainfos
+  var ul = document.createElement("ul")
+  ul.className = "ctooltip_meta";
+  container.appendChild(ul);
+
+  ul.style.float = "left";
+  ul.style["list-style"] = "none";
+  ul.style["padding-left"] = 0;
+
+  // preview SVG
+  var svgCont = document.createElement("div");
+  svgCont.className = "ctooltip_svg";
+  container.appendChild(svgCont);
+
+  svgCont.style.float = "right";
+
+  //------- adding 
+  document.body.appendChild(container);
+
+  //#### CONSTRUCTOR END ####
+
+
+  //#### methods ####
+  // add tootip to specific htmlElements
+  this.addTo = function(htmlElement) {
+    htmlElement.addEventListener("mouseenter", onMouseEnter);
+    htmlElement.addEventListener("mouseleave", onMouseLeave);
+    htmlElement.addEventListener("mousemove", onMouseMove);
+  }
+  this.removeFrom = function(htmlElement) {
+    htmlElement.removeEventListener("mouseenter", onMouseEnter);
+    htmlElement.removeEventListener("mouseleave", onMouseLeave);
+    htmlElement.removeEventListener("mousemove", onMouseMove);
+  }
+  this.setHeading = function(func) {
+    getHeading = func;
+  }
+  this.setSvg = function(func) {
+    getSvg = func;
+  }
+  this.setMetainfos = function (func) {
+    getMetainfos = func;
+  }
+  this.setStyle = function(styles) {
+    for (styleName in styles) {
+      container.style[styleName] = styles[styleName];
+    }
+  }
+
+  //#### private functions ####
+  function getHeading(event) {
+
+    return event.target.id;
+
+  }
+
+  function getSvg(event) {
+
+    console.log("CTooltip - getSvg: no function set to get an SVG, plz use .setSvg(function). Returning empty SVG.");
+    var emptySvg = document.createElementNS("http://www.w3.org/2000/svg","svg");
+    emptySvg.style.width = "190px";
+    emptySvg.style.height = "120px";
+    return emptySvg;
+
+  }
+
+  function getMetainfos(event) {
+
+    console.log("CTooltip - getMetainfos: no function set to get metainfos, plz use .setMetainfos(function).");
+
+    var metainfos = {
+                    firstInfo   : "this",
+                    secondInfo  : "is",
+                    thirdInfo   : "default"
+                    };
+    return metainfos;
+
+  }
+
+  function onMouseEnter(event) {
+
+    // set heading
+    heading.innerHTML = getHeading(event);
+
+    // set metainfos
+    ul.innerHTML = "";
+    var metainfos = getMetainfos(event);
+    for (metainfo in metainfos) {
+      var li = document.createElement("li");
+
+      var spanLabel = document.createElement("span");
+      spanLabel.innerHTML = metainfo;
+      spanLabel.style.float = "left";
+      spanLabel.style.width = "85px";
+
+      var spanInfo = document.createElement("span");
+      spanInfo.innerHTML = ": "+metainfos[metainfo];
+
+      li.appendChild(spanLabel);
+      li.appendChild(spanInfo);
+
+      ul.appendChild(li);
+    }
+
+    // set svg
+    var detachedSvg = getSvg(event);
+    var emptySvg = svgCont.cloneNode(false);
+    svgCont.parentNode.replaceChild(emptySvg, svgCont);
+    svgCont = emptySvg;
+    svgCont.appendChild(detachedSvg);
+    
+    // set position
+    var position = {  x: event.clientX,
+                      y: event.clientY };
+    show(position);
+
+  }
+
+  function onMouseLeave(event) {
+
+    hide();
+
+  }
+
+  function onMouseMove(event) {
+
+    var position = {  x: event.clientX,
+                      y: event.clientY };
+    show(position);
+
+  }
+
+  // show the tooltip
+  function show(pos) {
+
+    container.style.display = "initial";
+    container.style.left = pos.x+20+document.scrollingElement.scrollLeft+"px";
+    container.style.top = pos.y+document.scrollingElement.scrollTop+"px";
+    
+  }
+
+  // hide tooltip
+  function hide() {
+
+    container.style.display = "none";
+
+  }
+
+}
+
